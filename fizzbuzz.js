de79db1157a2a58aff116c468af0544792c1ba07
Ioana Gheorghias <ioana.gh27@gmail.com>
2022-07-12 15:28:16 +0100

    Add fizzbuzz function

---
+++ fizzbuzz.js
@@ -0,0 +1,16 @@
+const fizzbuzz = (number) => {
+    if(number % 3 == 0 && number % 5 == 0) {
+        return "Fizzbuzz";
+    } else if ( number % 3 == 0) {
+        return "Fizz";
+    } else if (number % 5 == 0) {
+        return "Buzz";
+    } else if (!number % 3 == 0 || number % 5 == 0){
+        return number;
+    } else {
+        throw "Error: missing inputs"
+    }
+}
+
+
+module.exports = fizzbuzz;
--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -1,5 +1,5 @@
 const each = require("jest-each").default;
-
+const fizzbuzz = require("../fizzbuzz");
 
 describe("fizzbuzz", () => {
     
@@ -21,9 +21,9 @@ describe("fizzbuzz", () => {
             [[15, 30, 45], "Fizzbuzz"],
             [[3, 9, 12], "Fizz"],
             [[5, 10, 50], "Buzz"],
-            [[4], 4]
+            [[4, 8, 13], 4]
         ]).test("%s -> %s", (arr, expected) => {
-            expect(fizzbuzz(...arr).toEqual(expected))
+            expect(fizzbuzz(...arr)).toEqual(expected)
         })
     })
 })
